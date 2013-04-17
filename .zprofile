81de113a4fdc3b81f60834e262b7fcf7054f3a8d
Patrick Yoon <yeyoon@cis.upenn.edu>
2013-04-16 20:06:13 -0400

    updated some aliases, added el-file

--- .zprofile
+++ .zprofile
@@ -11,10 +11,15 @@ alias eniac='ssh yeyoon@eniac.seas.upenn.edu'
 alias cis505='ssh cis505@eniac.seas.upenn.edu'
 alias shepard='ssh 158.130.51.23 -l yeyoon'
 alias shepardy='ssh -Y 158.130.51.23 -l yeyoon'
-alias wwbp='ssh yeyoon@wwbp.org'
+alias wwbp='ssh yeyoon@ssh.wwbp.org'
 alias rms='ssh yeyoon@rms.wharton.upenn.edu'
 alias emacsw='/Applications/Emacs.app/Contents/MacOS/Emacs'
 alias killemacs='kill `ps -A | grep Emacs | sed "s/^ *//g" | cut -f1 -d " "`'
+alias ..='cd ..'
+alias ...='cd ../../'
+alias ....='cd ../../..'
+alias ipython='ipython -nobanner -noconfirm_exit'
+alias less='less -F'
 test -r /sw/bin/init.sh && . /sw/bin/init.sh
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 #virtualenv
---
+++ patyoon.el
@@ -0,0 +1,392 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+;; list all packages that I want
+(setq my-el-get-packages
+      (append
+       '(css-mode
+         egg
+         gist
+         anything
+         yasnippet
+         zenburn
+         yaml-mode
+         yas-jit
+         virtualenv
+         sql-complete
+         slime
+         sass-mode
+         rvm
+         android-mode
+         auto-complete
+         clojure-mode
+         coffee-mode
+         ;;csharp-mode
+         csv-mode
+         cython-mode
+         django-mode
+         ein
+         flymake
+         flymake-coffee
+         flymake-css
+         flymake-ruby
+         flymake-haml
+         flymake-sass
+         go-mode
+         google-c-style
+         haskell-mode
+         haskell-latex
+         helm
+         ido-ubiquitous
+         ido-yes-or-no
+         jinja2-mode
+         jekyll-el
+         ;;jedi
+         magit
+         markdown-mode
+         ;;org
+         package
+         popup
+         ;;rope
+         ;;ropemacs
+         ruby-mode
+         ruby-compilation
+         smex
+         )
+       (mapcar 'el-get-source-name el-get-sources)))
+
+
+(el-get 'sync my-el-get-packages)
+
+;; Turn off icomplete, use helm instead
+(icomplete-mode 0)
+(setq prelude-whitespace nil)
+(defun disable-guru-mode ()
+  (guru-mode -1)
+)
+
+(add-hook 'prelude-prog-mode-hook 'disable-guru-mode t)
+(ido-ubiquitous 1)
+(setq prelude-guru nil)
+
+(require 'smex)
+(setq smex-history-length 100)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; Use Helm for buffer switching and file finding
+;; (global-set-key (kbd "\C-x b") 'helm-buffers-list)
+;; (global-set-key (kbd "\C-x \C-f") 'helm-find-files)
+
+;;Recentf stuff
+(add-to-list 'recentf-exclude "\\.ido\\.last")
+(add-to-list 'recentf-exclude "recentf")
+
+(global-set-key (kbd "\C-x f") 'prelude-recentf-ido-find-file)
+;; C-x b recent file finding
+(setq ido-use-virtual-buffers 1)
+
+;; Turn on linum mode
+(global-linum-mode t)
+(setq linum-format "%d ")
+;; for fixing broken fringes issue
+;; (fringe-mode 0)
+
+;;C mode setup
+(setq c-basic-offset 2)
+(add-hook 'prelude-c-mode-common-hook
+          '(lambda ()
+             (electric-pair-mode 0)
+             (c-set-style "gnu")))
+
+;; Eldoc in C mode
+(add-hook 'prelude-c-mode-hook 'c-turn-on-eldoc-mode)
+
+;;Command for sending to email (bound to C-c m)
+(defun mail-current-buffer ()
+  "Send the current buffer to email (for Mac)"
+  (interactive)
+  (shell-command (format "open -a Sparrow %s" (buffer-file-name))))
+(define-key global-map "\C-cm" 'mail-current-buffer)
+
+;;Make lines wrap instead of going over edge
+(global-visual-line-mode)
+
+;;Bind compile
+(global-set-key "\C-cl" 'compile)
+
+; From http://www.emacswiki.org/emacs/ParenthesisMatching#toc4
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis AND last command is a movement command, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (if (not (memq last-command '(
+                                set-mark
+                                cua-set-mark
+                                goto-match-paren
+                                down-list
+                                up-list
+                                end-of-defun
+                                beginning-of-defun
+                                backward-sexp
+                                forward-sexp
+                                backward-up-list
+                                forward-paragraph
+                                backward-paragraph
+                                end-of-buffer
+                                beginning-of-buffer
+                                backward-word
+                                forward-word
+                                mwheel-scroll
+                                backward-word
+                                forward-word
+                                mouse-start-secondary
+                                mouse-yank-secondary
+                                mouse-secondary-save-then-kill
+                                move-end-of-line
+                                move-beginning-of-line
+                                backward-char
+                                forward-char
+                                scroll-up
+                                scroll-down
+                                scroll-left
+                                scroll-right
+                                mouse-set-point
+                                next-buffer
+                                previous-buffer
+                                previous-line
+                                next-line
+                                )
+                 ))
+      (self-insert-command (or arg 1))
+    (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+          ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+          (t (self-insert-command (or arg 1))))))
+(global-set-key (kbd "%") 'goto-match-paren)
+
+;; Newline and indent everywhere
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+;; Reindent on yank
+;; http://emacswiki.org/emacs/AutoIndentation
+;; (dolist (command '(yank yank-pop))
+;;   (eval `(defadvice ,command (after indent-region activate)
+;;            (and (not current-prefix-arg)
+;;                 (member major-mode '(emacs-lisp-mode lisp-mode
+;;                                                      clojure-mode    scheme-mode
+;;                                                      ruby-mode
+;;                                                      rspec-mode
+;;                                                      c-mode          c++-mode
+;;                                                      objc-mode       latex-mode
+;;                                                      plain-tex-mode java-mode))
+;;                 (let ((mark-even-if-inactive transient-mark-mode))
+;;                   (indent-region (region-beginning) (region-end) nil))))))
+
+;; Delete extra whitespace when killing lines
+(defun kill-and-join-forward (&optional arg)
+  "If at end of line, join with following; otherwise kill line.
+    Deletes whitespace at join."
+  (interactive "P")
+  (if (and (eolp) (not (bolp)))
+      (delete-indentation t)
+    (kill-line arg)))
+(global-set-key "\C-k" 'kill-and-join-forward)
+
+;; Elisp useful stuff
+;;(add-hook 'prelude-emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+
+;;Autocomplete mode
+;;Supposedly makes nicer autocomplete rendering
+;;(require) 'pos-tip
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
+;;(global-auto-complete-mode t)
+(ac-config-default)
+;;Needed for compatibility with flyspell
+;;(ac-flyspell-workaround)
+(setq ac-auto-start 3)
+(setq ac-auto-show-menu t)
+(setq ac-quick-help-delay .3)
+(ac-set-trigger-key "TAB")
+;; Make \C-n and \C-p work in autocompletion menu
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
+
+;;More package archives
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; Buffer movement by arrow keys
+;; (require 'buffer-move)
+;; (global-set-key (kbd "<up>")     'buf-move-up)
+;; (global-set-key (kbd "<down>")   'buf-move-down)
+;; (global-set-key (kbd "<left>")   'buf-move-left)
+;; (global-set-key (kbd "<right>")  'buf-move-right)
+
+;;CMake mode
+;;(require 'cmake-mode)
+(setq auto-mode-alist
+      (append '(("CMakeLists\\.txt\\'" . cmake-mode)
+                ("\\.cmake\\'" . cmake-mode))
+              auto-mode-alist))
+(setq auto-mode-a)
+
+;;Disable flymake GUI warnings (they cause crash)
+(setq flymake-gui-warnings-enabled nil)
+
+;; Scroll to end of window before error
+(setq scroll-error-top-bottom t)
+
+;; Ido-ubiquitous broken in M-x man, disable it
+(add-to-list 'ido-ubiquitous-command-exceptions 'man)
+(ido-ubiquitous-disable-in man)
+
+;;Email setup
+(setq user-mail-address "kinetoz@gmail.com")
+      (setq user-full-name "Patrick Yoon")
+(setq smtpmail-smtp-user "kinetoz")
+(setq smtpmail-smtp-server "smtp.gmail.com")
+(setq mail-user-agent 'message-user-agent)
+(setq message-send-mail-function 'smtpmail-send-it)
+(setq make-backup-files nil) ; stop creating those backup~ files
+(setq backup-by-copying t)
+(global-subword-mode 1) ;move thru camelCaseWords
+
+;;(server-start)
+
+;; fontify code in code blocks
+(setq org-src-fontify-natively t)
+
+(defun disable-guru-mode ()
+  (guru-mode -1)
+  )
+
+(add-hook 'prelude-prog-mode-hook 'disable-guru-mode t)
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+
+;;hyde
+(defun hyde-new-draft()
+  "Init a new blog post for hyde engine"
+  (interactive)
+  (insert "{% extends \"_post.html\" %}\n{%hyde\n   title:\n   categories:\n   created: ")
+  (insert (format-time-string "%Y-%m-%d"))
+  (insert "\n%}\n{% block article %}\n{% article %}\n\n{% endarticle %}\n{% endblock %}")
+  (goto-line 9)
+  )
+
+(add-to-list 'auto-mode-alist '("\\.l[gh]s\\'" . haskell-latex-mode))
+(autoload 'haskell-latex-mode "haskell-latex")
+
+(defun next-user-buffer ()
+    "Switch to the next user buffer.
+User buffers are those whose name does not start with *."
+      (interactive)
+        (next-buffer)
+          (let ((i 0))
+                (while (and (string-match "^*" (buffer-name)) (< i 50))
+                        (setq i (1+ i)) (next-buffer))))
+
+(defun previous-user-buffer ()
+    "Switch to the previous user buffer.
+User buffers are those whose name does not start with *."
+      (interactive)
+        (previous-buffer)
+          (let ((i 0))
+                (while (and (string-match "^*" (buffer-name)) (< i 50))
+                        (setq i (1+ i)) (previous-buffer))))
+
+(defun next-emacs-buffer ()
+    "Switch to the next emacs buffer.
+Emacs buffers are those whose name starts with *."
+      (interactive)
+        (next-buffer)
+          (let ((i 0))
+                (while (and (not (string-match "^*" (buffer-name))) (< i 50))
+                        (setq i (1+ i)) (next-buffer))))
+
+(defun previous-emacs-buffer ()
+    "Switch to the previous emacs buffer.
+Emacs buffers are those whose name starts with *."
+      (interactive)
+        (previous-buffer)
+          (let ((i 0))
+                (while (and (not (string-match "^*" (buffer-name))) (< i 50))
+                        (setq i (1+ i)) (previous-buffer))))
+
+;; sample easy keys
+
+(global-set-key (kbd "<f5>") 'load-file) ; Load file
+(global-set-key (kbd "<f6>") 'package-list-packages) ; list packages
+
+(global-set-key (kbd "<C-prior>") 'previous-user-buffer) ; Ctrl+PageUp
+(global-set-key (kbd "<C-next>") 'next-user-buffer) ; Ctrl+PageDown
+(global-set-key (kbd "<C-S-prior>") 'previous-emacs-buffer) ; Ctrl+Shift+PageUp
+(global-set-key (kbd "<C-S-next>") 'next-emacs-buffer) ; Ctrl+Shift+PageDown
+(custom-set-variables
+
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(ansi-term-color-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "50be9f1476216575cabae8717557fca94642f8ca9c72b4bae09b9f1606233c67" default)))
+ '(fci-rule-color "#383838"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; for evernote mode
+(setq evernote-username "patryoon") ; optional: you can use this username as default.
+(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8")) ; option
+(global-set-key "\C-cec" 'evernote-create-note)
+(global-set-key "\C-ceo" 'evernote-open-note)
+(global-set-key "\C-ces" 'evernote-search-notes)
+(global-set-key "\C-ceS" 'evernote-do-saved-search)
+(global-set-key "\C-cew" 'evernote-write-note)
+(global-set-key "\C-cep" 'evernote-post-region)
+(global-set-key "\C-ceb" 'evernote-browser)
+
+;; python mode tab width
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (setq indent-tabs-mode nil
+                            tab-width 4))))
+
+;; now either el-get is `require'd already, or have been `load'ed by the
+;; el-get installer.
+(add-to-list 'el-get-recipe-path (expand-file-name "~/prg/el-get/recipes"))
+
+(require 'package)
+(package-initialize)
+
+;;(add-hook 'python-mode-hook 'jedi:setup)
+
+;;(setq jedi:setup-keys t)
+
+;; ipython notebook
+;;(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(add-to-list 'load-path "~/.emacs.d/vendor/arduino")
+(setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
+(autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
+
+(setq TeX-PDF-mode t)
