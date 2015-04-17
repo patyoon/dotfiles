8f632097049e5d582d02c452f64c09cf69c5b788
Patrick Yoon <patrick.yoon@airbnb.com>
2015-04-17 14:54:58 -0700

    [emacs] Adding eclim

--- emacs/partials/java.el
+++ emacs/partials/java.el
@@ -17,5 +17,13 @@
 (setq eclim-auto-save t)
 (global-eclim-mode)
 
+(setq help-at-pt-display-when-idle t)
+(setq help-at-pt-timer-delay 0.1)
+(help-at-pt-set-timer)
+
+(custom-set-variables
+ '(eclim-eclipse-dirs '("~/nonStandard/eclipse"))
+ '(eclim-executable "~/nonStandard/eclipse/eclim"))
+
 (provide `java)
 ;;; java.el ends here.
